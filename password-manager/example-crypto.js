b84e3a7542a8d3e7c826577b097e5637fce9a0f1
crazycoderTR <kilincaslanmesut@gmail.com>
2018-10-27 18:41:19 +0300

    Modified something

---
+++ password-manager/example-crypto.js
@@ -0,0 +1,15 @@
+"use strict";
+/*---------------------------------------- Variable Definition ----------------------------------------*/
+const crypto = require("crypto-js");
+let message = "this is a message";
+let key = "123";
+/*---------------------------------------- Variable Definition ----------------------------------------*/
+
+// Encrypt
+let encrytedMessage = crypto.AES.encrypt(message, key);
+console.log(`Encryped Message => ${encrytedMessage}`);
+
+// Decrypt
+let bytes = crypto.AES.decrypt(encrytedMessage, key);
+let decryptedMessage = bytes.toString(crypto.enc.Utf8);
+console.log(`Decrypted Message => ${decryptedMessage}`);
---
+++ password-manager/example-yargs.js
@@ -0,0 +1,25 @@
+"use strict";
+/*---------------------------------------- Variable Definition ----------------------------------------*/
+const argv = require('yargs')
+	.command('hello', 'Say Hi to users!', function(yargs){
+		yargs.options({
+			name: {
+				demand: true,
+				description: 'It is name variable...',
+				alias: 'n',
+				type: 'string'
+			},
+			surname: {
+				demand: true,
+				description: 'It is surname variable...',
+				alias: 'n',
+				type: 'string'
+			}
+		}).help('help');
+	}).help('help').argv;
+/*---------------------------------------- Variable Definition ----------------------------------------*/
+
+/*--------------------- First use of yargs ---------------------*/
+if(argv._[0] === 'hello' && typeof argv.name !== 'undefined' && argv.surname !== 'undefined'){
+	console.log('Welcome ' + argv.name + ' ' + argv.surname);
+}
